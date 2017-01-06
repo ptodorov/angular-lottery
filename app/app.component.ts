b364e0d666c6e752fd43e27132a94e2974f636a2
Panayot Todorov <Panayot Todorov>
2017-01-06 10:39:03 +0200

    new version of typescript and style changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,28 +7,38 @@ import { Prize } from './models/Prize';
   styleUrls: ['./app/app.component.css']
 })
 export class AppComponent  {
-  private firstSelectedValue: number = 1;
-  private secondSelectedValue: number = 1;
-  private thirdSelectedValue: number = 1;
+  private firstColumnPrize: Prize;
+  private secondColumnPrize: Prize;
+  private thirdColumnPrize: Prize;
 
-  private prizeList: Prize[];
+  private prizeList: Prize[] = [];
+
+  private isReady: boolean = true;
 
   constructor() {
     this.prizeList.push(new Prize(1, "./app/assets/images/Jakey.png"));
     this.prizeList.push(new Prize(2, "./app/assets/images/Vesko.png"));
     this.prizeList.push(new Prize(3, "./app/assets/images/Jakey.png"));
+
+    this.firstColumnPrize = this.prizeList[0];
+    this.secondColumnPrize = this.prizeList[1];
+    this.thirdColumnPrize = this.prizeList[2];
   }
 
   private wait(timeoutInMilliseconds: number) {
       return new Promise(resolve => setTimeout(resolve, timeoutInMilliseconds));
   }
 
-  async startSlotMachine() { 
+  async startSlotMachine() {
+      this.isReady = false;
+
       for (let i = 0; i < 10; i++) {
-          this.firstSelectedValue = Math.floor(Math.random() * 3 + 1);
-          this.secondSelectedValue = Math.floor(Math.random() * 3 + 1);
-          this.thirdSelectedValue = Math.floor(Math.random() * 3 + 1);
+          this.firstColumnPrize = this.prizeList[Math.floor(Math.random() * 3)];
+          this.secondColumnPrize = this.prizeList[Math.floor(Math.random() * 3)];
+          this.thirdColumnPrize = this.prizeList[Math.floor(Math.random() * 3)];
           await this.wait(200);
       }
+
+      this.isReady = true;
   }
 }
